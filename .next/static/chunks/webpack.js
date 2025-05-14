b1a251112b0dd4e672f51c6c1924fa5db8ac24d7
2032emegre <mamepluslab@gmail.com>
2025-05-14 16:05:40 +0900

    fix: 記録一覧の仮データを廃止しローディング表示を追加（初期表示のちらつき解消）

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fa34ddc649089312"; }
+/******/ 		__webpack_require__.h = function() { return "f4484c59f02b49dc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
